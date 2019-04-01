6b77f2b723d809bb1c3277cc693526127f6a3c6a
CarineIzere <carizeree@gmail.com>
2019-04-01 13:23:53 +0200

    add models.py

--- rate/models.py
+++ rate/models.py
@@ -2,12 +2,48 @@ from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
+class Project(models.Model):
+    title = models.CharField(max_length = 60,null = True)
+    image = models.ImageField(upload_to = "images/",null = True)
+    user = models.ForeignKey(User,null=True)
+    
+    link = models.CharField(max_length = 70,null = True)
+    description = models.TextField(null = True)
+    pub_date = models.DateTimeField(auto_now_add=True,null=True)
+    profile = models.ForeignKey(Profile, null=True) 
+  
+
+
+    def __str__(self):
+        return self.image
+
+    def delete_image(self):
+        self.delete()
+
+    def save_image(self):
+        self.save()
+
+    def update_description(self,new_description):
+        self.image_description = new_description
+        self.save()
+
+
+    @classmethod
+    def get_image(cls, id):
+        image = cls.objects.get(id=id)
+        return image
+
+
+
+ class Meta:
+        ordering = ['-pub_date']
 class Profile(models.Model):
 	username = models.CharField(default='User',max_length=30)
 	profile_picture = models.ImageField(upload_to = "profile/",null=True)
 	bio = models.TextField(default='',blank = True)
-	first_name = models.CharField(max_length =30)
-	last_name = models.CharField(max_length =30)
+	project = models.ForeignKey(Project,null=True)
+    contact = models.TextField(null = True)
+    project = models.IntegerField(default=0)
 
 	def __str__(self):
 		return self.username
@@ -18,10 +54,13 @@ class Profile(models.Model):
 	def save_profile(self):
 		self.save()
 
-	@classmethod
-	def search_profile(cls,search_term):
-		got_profiles = cls.objects.filter(first_name__icontains = search_term)
-		return got_profiles
+	# @classmethod
+	# def search_profile(cls,search_term):
+	# 	got_profiles = cls.objects.filter(first_name__icontains = search_term)
+	# 	return got_profiles
+
+     class Meta:
+        ordering = ['-pub_date']
 		
 class Image(models.Model):
     image = models.ImageField(upload_to = "images/",null = True)
@@ -44,60 +83,72 @@ class Image(models.Model):
     def update_caption(self,new_caption):
     	self.image_caption = new_caption
     	self.save()
-class Comment(models.Model):
-	user = models.ForeignKey(User,on_delete=models.CASCADE)
-	image= models.ForeignKey(Image,on_delete=models.CASCADE,null= True,related_name='comment')
-	comment= models.TextField( blank=True)
+
+# class Comment(models.Model):
+# 	user = models.ForeignKey(User,on_delete=models.CASCADE)
+# 	image= models.ForeignKey(Image,on_delete=models.CASCADE,null= True,related_name='comment')
+# 	comment= models.TextField( blank=True)
 	
-	def __str__(self):
-		return self.comment
+# 	def __str__(self):
+# 		return self.comment
 
 
-	def delete_comment(self):
-		self.delete()
+# 	def delete_comment(self):
+# 		self.delete()
 
-	def save_comment(self):
-		self.save()
+# 	def save_comment(self):
+# 		self.save()
 
-class Follow(models.Model):
-	user = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
-	follower = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+# class Follow(models.Model):
+# 	user = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
+# 	follower = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
 
-	def __int__(self):
-		return self.name
+# 	def __int__(self):
+# 		return self.name
 
-	def save_follower(self):
-		self.save()
+# 	def save_follower(self):
+# 		self.save()
 
-	def delete_follower(self):
-		self.save()
+# 	def delete_follower(self):
+# 		self.save()
 
-class Unfollow(models.Model):
-	user = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
-	follower = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+# class Unfollow(models.Model):
+# 	user = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
+# 	follower = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
 
-	def __int__(self):
-		return self.name
+# 	def __int__(self):
+# 		return self.name
 
-class Likes(models.Model):
-	user = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
+# class Likes(models.Model):
+# 	user = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
 	
 
-	def __int__(self):
-		return self.name
+# 	def __int__(self):
+# 		return self.name
 
-	def unlike(self):
-		self.delete()
+# 	def unlike(self):
+# 		self.delete()
 
-	def save_like(self):
-		self.save()
+# 	def save_like(self):
+# 		self.save()
 
-# Create your models here.
-# class Rating(models.Model):
-#     design = models.IntegerField(blank=True,default=0)
-#     usability = models.IntegerField(blank=True,default=0)
-#     creativity = models.IntegerField(blank=True,default=0)
-#     content = models.IntegerField(blank=True,default=0)
-#     overall_score = models.IntegerField(blank=True,default=0)
-#     project = models.ForeignKey(Project,on_delete=models.CASCADE)
-#     profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+Create your models here.
+class Rating(models.Model):
+    user = models.ForeignKey(User, null= True)
+    design = models.IntegerField(blank=True,default=0)
+    usability = models.IntegerField(blank=True,default=0)
+    project = models.ForeignKey(Project, null= True,related_name='rating')
+    content = models.IntegerField(blank=True,default=0)
+    overall_score = models.IntegerField(blank=True,default=0)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+
+def __str__(self):
+        return self.content
+
+
+    def delete_content(self):
+        self.delete()
+
+    def save_content(self):
+        self.save()
